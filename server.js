e7340cae8a7be308ca6c59e6db43c6f84a7191d6
Dorian Dubreus <doriandubreus@gmail.com>
2022-12-16 12:48:12 -0500

    connecting to database and adding models

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
-const { logger } = require('./middleware/logger')
+const { logger, logEvents } = require('./middleware/logger')
 const errorHandler = require('./middleware/errorHandler')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3000
 
 
+connectDB()
 
 app.use(logger)
 
@@ -38,6 +42,16 @@ app.all('*', (req, res) => {
 
 app.use(errorHandler)
 
-app.listen(PORT, () => {
-    console.log('server is running better catch it')
+mongoose.connection.once('open', () => {
+    console.log('connected to mongoDB')
+    app.listen(PORT, () => {
+        console.log('server is running better catch it')
+    })
+
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+        'mongoErrLog.log')
 })
